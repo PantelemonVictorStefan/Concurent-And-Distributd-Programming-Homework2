a896a161c3b306499a84daf97fdb350109ae68f2
Razvan BAISAN <rbaisan@bitdefender.com>
2020-03-17 13:43:37 +0200

    initial commit; angular app

---
+++ cdp-app/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { PostItemComponent } from './components/post-item/post-item.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PostsComponent,
+    PostItemComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ cdp-app/src/app/models/Post.ts
@@ -0,0 +1,7 @@
+export class Post {
+    id:string;
+    title:string;
+    description:string;
+    url:string;
+    imgurl:string
+}
